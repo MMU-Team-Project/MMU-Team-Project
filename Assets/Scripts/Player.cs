12100c5c50739bed94e7eee8cbd338d54dc7a6e0
JoeAnderton <joe.anderton07@gmail.com>
2026-02-11 16:00:57 +0000

    Added basic movement
    Player can move forward, backward and diagonally. Camera is set to first person.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    int playerSpeed = 5;
+    Rigidbody rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        if (horizontal != 0 && vertical != 0)
+        {
+            rb.linearVelocity = new Vector3((playerSpeed / Mathf.Sqrt(2)) * horizontal, rb.linearVelocity.y, (playerSpeed / Mathf.Sqrt(2)) * vertical);
+        }
+        else 
+        {
+            rb.linearVelocity = new Vector3(playerSpeed * horizontal, rb.linearVelocity.y, playerSpeed * vertical);
+        }
+    }
+}
