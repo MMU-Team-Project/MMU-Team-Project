37eab2305db71cf4dc7c50dffde529ea3f116dc6
K-Z-Elliott <235063678+K-Z-Elliott@users.noreply.github.com>
2026-02-19 03:02:01 +0000

    Staff shoot functionality
    Staff now shoots a bullet that homes toward where you are looking while continuously travelling forward.
    Also added some simple animations.

---
+++ Assets/Prefabs/Weapons/Staves/Scripts/MagicMissile.cs
@@ -0,0 +1,48 @@
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+
+public class MagicMissile : MonoBehaviour
+{
+    private float homingForce = 20f;
+    private float maxSpeed = 100f;
+    private float damping = 1f;
+
+    private float targetDistance = 1f;
+    private float targetSpeed = 3f;
+
+    private Rigidbody rb;
+    private Camera playerCam;
+    private Vector3 homingTarget;
+
+
+    public void Setup(Camera newPlayerCam)
+    {
+        playerCam = newPlayerCam;
+        rb = transform.GetComponent<Rigidbody>();
+        homingTarget = playerCam.transform.position + playerCam.transform.forward * targetDistance;
+    }
+
+    void FixedUpdate()
+    {
+        if (playerCam != null && rb != null)
+        {
+            //Moves the homingTarget away from the player over time
+            targetDistance += targetSpeed * Time.fixedDeltaTime;
+            homingTarget = playerCam.transform.position + playerCam.transform.forward * targetDistance;
+
+            //Adds force toward the homingTarget
+            Vector3 toTarget = homingTarget - transform.position;
+            rb.AddForce(toTarget.normalized * homingForce, ForceMode.Acceleration);
+
+            //Prevents ridiculous unbound speeds
+            if (rb.linearVelocity.magnitude > maxSpeed)
+            {
+                rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
+            }
+
+            //Slows the missile as it gets toward its goal
+            Vector3 dampingForce = -rb.linearVelocity * damping;
+            rb.AddForce(dampingForce, ForceMode.Acceleration);
+        }
+    }
+}
