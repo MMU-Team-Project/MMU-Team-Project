d325bccb688aed18e03c84937678ac0a345c4a0f
WallJam235 <wallerjames235@gmail.com>
2026-02-24 06:15:17 +0000

    Floating Healthbar + basic loot drop
    Enemies now have a floating health  that tracks their health and faces the camera, also added a basic item drop system which currently has a 50% chance to spawn a placeholder item on enemy death.

---
+++ Assets/Scripts/EnemyScripts/HealthBarUI.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarUI : MonoBehaviour
+{
+
+    public float Health, MaxHealth, Width, Height;
+
+    [SerializeField]
+    private RectTransform healthBar;
+
+    [SerializeField]
+    private Canvas canvas;
+
+    private Camera cam;
+
+    void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    public void SetMaxHealth(float maxHealth)
+    {
+        MaxHealth = maxHealth;
+    }
+
+    public void SetHealth(float health)
+    {
+        Health = health;
+        float newWidth = (Health / MaxHealth) * Width; //based off CurrentHealth's width/height to allow for border on UI
+
+        healthBar.sizeDelta = new Vector2(newWidth, Height);
+    }
+
+    void Update()
+    {
+        var toCam = canvas.transform.position - cam.transform.position;
+        canvas.transform.rotation = Quaternion.LookRotation(toCam, Vector3.up);
+    }
+}
