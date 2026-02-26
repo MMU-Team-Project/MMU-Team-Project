6f739ffb268fc730776c841841aa5f3f91162189
TryRexT1 <thatdudeagainlin@gmail.com>
2026-02-26 19:08:34 +0000

    Bow Model Update

--- Assets/Prefabs/Weapons/Bows/Scripts/Bow.cs
+++ Assets/Prefabs/Weapons/Bows/Scripts/Bow.cs
@@ -1,7 +1,35 @@
 using UnityEngine;
 
-public class Bow : MonoBehaviour
+public class Bow : MonoBehaviour, IWeapon
 {
+    private GameObject player;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private Vector3 rotation;
+    private Camera playerCam;
+
+    public void SetPlayer(GameObject newPlayer)
+    {
+        if (player == null)
+        {
+            player = newPlayer;
+        }
+        //Set player owner if there isn't already one
+    }
+
+    public void Equip(GameObject newPlayer)
+    {
+        SetPlayer(newPlayer);
+        transform.SetParent(player.transform, false);
+        transform.localPosition = offset;
+        transform.localRotation = Quaternion.Euler(rotation);
+        playerCam = player.transform.GetComponentInChildren<Camera>();
+    }
+
+    public void Use()
+    {
+
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
