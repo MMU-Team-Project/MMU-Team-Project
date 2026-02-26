c1f7cb2ac7c26959ab79cc0912fa5d0691c78b66
TryRexT1 <thatdudeagainlin@gmail.com>
2026-02-26 19:59:43 +0000

    Bow Updates

--- Assets/Prefabs/Weapons/Bows/Scripts/Bow.cs
+++ Assets/Prefabs/Weapons/Bows/Scripts/Bow.cs
@@ -2,18 +2,20 @@ using UnityEngine;
 
 public class Bow : MonoBehaviour, IWeapon
 {
+    // Data Members
     private GameObject player;
+    private float offCDShoot = 0f;
+    private Camera playerCam;
     [SerializeField] private Vector3 offset;
     [SerializeField] private Vector3 rotation;
-    private Camera playerCam;
 
     public void SetPlayer(GameObject newPlayer)
     {
+        // Set player owner if there isn't already one
         if (player == null)
         {
             player = newPlayer;
         }
-        //Set player owner if there isn't already one
     }
 
     public void Equip(GameObject newPlayer)
@@ -27,18 +29,23 @@ public class Bow : MonoBehaviour, IWeapon
 
     public void Use()
     {
-
+        if (player != null)
+        {
+            if (Time.time < offCDShoot)
+            {
+                Debug.Log("Cooldown!");
+                return;
+            }
+        }
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
