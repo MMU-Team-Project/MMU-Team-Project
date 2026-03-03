329c2b13525909039e3119b8d47ab069444d19e2
JoeAnderton <joe.anderton07@gmail.com>
2026-03-03 16:12:35 +0000

    The sprint sprint
    - Added sprinting and doubled regular walking speed.
    -Added a camera bobbing animation when walking.
    - Adjusted camera FOV so objects on the edge of the lens don't look stretched out.

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -3,12 +3,11 @@ using UnityEngine;
 public class Camera : MonoBehaviour
 {
     public float currentRotation = 0;
-    public float currentXRotation= 0;
     public float rotationSpeed = 1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -28,28 +27,5 @@ public class Camera : MonoBehaviour
             currentRotation = currentRotation - rotationSpeed * Input.GetAxis("Mouse Y");
             transform.localRotation = Quaternion.AngleAxis(currentRotation, Vector3.right);
         }
-        //if (Input.GetAxis("Mouse X") < 0)
-        //{
-        //    currentXRotation = currentXRotation + rotationSpeed * Input.GetAxis("Mouse X");
-        //    transform.rotation = Quaternion.AngleAxis(currentXRotation, Vector3.up);
-        //}
-        //if (Input.GetAxis("Mouse X") > 0)
-        //{
-        //    currentXRotation = currentXRotation + rotationSpeed * Input.GetAxis("Mouse X");
-        //    transform.rotation = Quaternion.AngleAxis(currentXRotation, Vector3.up);
-        //}
     }
-    //void FixedUpdate()
-    //{
-    //    if (Input.GetAxis("Mouse Y") < 0 && currentRotation >= -70)
-    //    {
-    //        currentRotation = currentRotation - rotationSpeed * Input.GetAxis("Mouse Y");
-    //        transform.localRotation = Quaternion.AngleAxis(currentRotation,Vector3.right);
-    //    }
-    //    if (Input.GetAxis("Mouse Y") > 0 && currentRotation <= 70)
-    //    {
-    //        currentRotation = currentRotation - rotationSpeed * Input.GetAxis("Mouse Y");
-    //        transform.localRotation = Quaternion.AngleAxis(currentRotation,Vector3.right);
-    //    }
-    //}
 }
