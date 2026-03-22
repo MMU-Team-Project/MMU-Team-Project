020ba0c4e01d0ba441b5e630cf29ecc26368a796
K-Z-Elliott <235063678+K-Z-Elliott@users.noreply.github.com>
2026-03-22 00:01:53 +0000

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -11,6 +12,10 @@ public class Player : MonoBehaviour
     public bool grounded = true;
     public bool jumping = false;
     public bool walking = false;
+    public bool hit = false;
+    public float health = 100;
+    private bool Hitting = false;
+    public GameObject HittingEnemy;
     Animator animator;
     Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -24,8 +29,10 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        animator.SetBool("Is Walking", walking);
-        if (Input.GetButtonDown("Jump") && (grounded||jumping))
+
+        //line below results in syntax error; stops rest of the Update() function from running. Disabled for fixes.
+        //animator.SetBool("Is Walking", walking);
+        if (Input.GetButtonDown("Jump") && (grounded || jumping))
         {
             rb.linearVelocity += transform.up * jumpSpeed;
             jumping = true;
@@ -80,12 +87,54 @@ public class Player : MonoBehaviour
         {
             rb.linearVelocity += transform.up * jumpSpeed * Time.deltaTime * Physics.gravity.y;
         }
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+        if (hit)
+        {
+            StartCoroutine(HitByEnemy(hit));
+        }
     }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.layer == 3)
         {
             grounded = true; //checks if on the ground
+        }
+        if (collision.gameObject.tag == "Enemy")
+        {
+            hit = true;
+            HittingEnemy = collision.gameObject;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            hit = false;
+            StopCoroutine(HitByEnemy(hit));
+        }
+    }
+
+    IEnumerator HitByEnemy(bool hit)
+    {
+        if (hit)
+        {
+            if (Hitting == false)
+            {
+                Hitting = true;
+                float damage = HittingEnemy.GetComponent<EnemyHealth>().Damage;
+                health -= damage;
+                print("Player hit for " + damage + "health. From " + (health + damage) + " down to " + health);
+                yield return new WaitForSeconds(1);
+                Hitting = false;
+                if (!hit)
+                {
+                    StopCoroutine(HitByEnemy(hit));
+
+                }
+            }
         }
     }
 }
